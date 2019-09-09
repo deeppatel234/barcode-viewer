236a583fdefeff1cac7686873815fd53ce0a20e4
Deep Patel <deep.patel@agrostar.in>
2019-09-09 21:38:33 +0530

    Updates

---
+++ precache-manifest.1e22e20ef218eb38ae56ac3c4b3e3a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce6eb793fcede3876c902a91169f47b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30af10595e58c98c244d",
+    "url": "/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "f557992992d1486fcecc",
+    "url": "/static/css/main.816d4bdf.chunk.css"
+  },
+  {
+    "revision": "30af10595e58c98c244d",
+    "url": "/static/js/2.9f4c7a86.chunk.js"
+  },
+  {
+    "revision": "f557992992d1486fcecc",
+    "url": "/static/js/main.09a3a805.chunk.js"
+  },
+  {
+    "revision": "2330e5964aec47b351eb",
+    "url": "/static/js/runtime~main.1ea6d4db.js"
+  }
+]);
